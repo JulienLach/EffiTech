f973578f6ecbea56bfad0d9624eaa8ee6c87b960
Julien Lach <leadedchamps@gmail.com>
2024-08-09 23:19:52 +0200

    added createAddress method in controller

--- backend/controllers/address.controller.js
+++ backend/controllers/address.controller.js
@@ -8,3 +8,12 @@ exports.getAddressById = async (req, res) => {
         res.status(500).json({ message : 'Erreur lors de la récupération de l\'adresse', error: error.message});
     }
 };
+
+exports.createAddress = async (req, res) => {
+    try {
+        const address = await Address.createAddress(req.body.address, req.body.city, req.body.zipcode);
+        res.status(201).json(address);
+    } catch (error) {
+        res.status(500).json({ message : 'Erreur lors de la création de l\'adresse', error: error.message});
+    }
+};
