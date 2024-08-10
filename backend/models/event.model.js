0af772e3d617bcd804c31571a5d0564f9ffa85e4
Julien Lach <leadedchamps@gmail.com>
2024-08-10 18:39:44 +0200

    added methods for event class

--- backend/models/event.model.js
+++ backend/models/event.model.js
@@ -20,6 +20,31 @@ class Event {
         const result = await pool.query('SELECT * FROM events');
         return result.rows;
     }
+
+    static async getEventById(idEvent) {
+        const query = 'SELECT * FROM employees WHERE idEmployee = $1';
+        const values = [idEvent];
+        const result = await pool.query(query, values);
+        return result.rows[0];
+    }
+
+    static async createEvent(title, description, status, isPlanned, type, idClient, idAddress, startingDate, startingHour, endingHour, idEmployee) {
+        const query = 'INSERT INTO employees (title, description, status, isPlanned, type, idClient, idAddress, startingDate, startingHour, endingHour, idEmployee) VALUES ()';
+        const values = [title, description, status, isPlanned, type, idClient, idAddress, startingDate, startingHour, endingHour, idEmployee];
+        await pool.query(query, values);
+    }
+
+    static async updateEvent(title, description, status, isPlanned, type, idClient, idAddress, startingDate, startingHour, endingHour, idEmployee, idEvent) {
+        const query = 'UPDATE events SET title = $1, description = $2, status = $3, isPlanned = $4, type = $5, idClient = $6, idAddress = $7, startingDate = $8, startingHour = $9, endingHour = $10, idEmployee = $11 WHERE idEvent = $12';
+        const values = [title, description, status, isPlanned, type, idClient, idAddress, startingDate, startingHour, endingHour, idEmployee, idEvent];
+        await pool.query(query, values);
+    }
+
+    static async deleteEvent(idEvent) {
+        const query = 'DELETE FROM events WHERE idEvent = $1';
+        const values = [idEvent];
+        await pool.query(query, values);
+    }
 }
 
 class Appointment extends Event {
