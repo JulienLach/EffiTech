4169223636352c0339f50b40df213f61979a727f
Julien Lach <leadedchamps@gmail.com>
2024-08-15 08:43:12 +0200

    refactored node modules for frontend and backend, added first homepage test

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Page d'accueil</h1>
+      <p>test paragraphe</p>
+    </div>
+  );
+};
+
+export default HomePage;
