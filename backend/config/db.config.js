621d2f14fc830778d16b9bc00d943b361ccc67cc
JulienLach <leadedchamps@gmail.com>
2024-08-19 16:17:40 +0200

    added error handling when connecting to postreSQL

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -15,4 +15,8 @@ pool.on('connect', () => {
   console.log('Connected to the PostgreSQL database.');
 });
 
+pool.on('error', (err) => {
+  console.error('Error connecting to the PostgreSQL database:', err);
+});
+
 module.exports = pool;
