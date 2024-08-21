45107f016c6293380b5ff096e7826f6cdf27b942
JulienLach <leadedchamps@gmail.com>
2024-08-21 11:41:00 +0200

    refactored employee data with object in callback

--- backend/data/employee.data.js
+++ backend/data/employee.data.js
@@ -19,7 +19,8 @@ class Employee {
             if (error) {
                 return callback(error, null);
             }
-            callback(null, result.rows);
+            const employees = result.rows.map(row => new Employee(row.idEmployee, row.firstname, row.lastname, row.job, row.phoneNumber, row.email, row.isAdmin, row.password, row.speciality));
+            callback(null, employees);
         });
     }
 
@@ -30,29 +31,35 @@ class Employee {
             if (error) {
                 return callback(error, null);
             }
-            callback(null, result.rows[0]);
+            const row = result.rows[0];
+            let employee = new Employee(row.idEmployee, row.firstname, row.lastname, row.job, row.phoneNumber, row.email, row.isAdmin, row.password, row.speciality);
+            callback(null, employee);
         });
     }
 
     static createEmployee(firstname, lastname, job, phoneNumber, email, isAdmin, password, speciality, callback) {
         const query = 'INSERT INTO employees (firstname, lastname, job, phoneNumber, email, isAdmin, password, speciality) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
         const values = [firstname, lastname, job, phoneNumber, email, isAdmin, password, speciality];
-        pool.query(query, values, (error, result) => {
+        pool.query(query, values, (error, newEmployee) => {
             if (error) {
                 return callback(error, null);
             }
-            callback(null, result);
+            const row = newEmployee.rows[0];
+            newEmployee = new Employee(row.idEmployee, row.firstname, row.lastname, row.job, row.phoneNumber, row.email, row.isAdmin, row.password, row.speciality);
+            callback(null, newEmployee);
         });
     }
     
     static updateEmployee(idEmployee, firstname, lastname, job, phoneNumber, email, isAdmin, password, speciality, callback) {
         const query = 'UPDATE employees SET firstname = $1, lastname = $2, job = $3, phoneNumber = $4, email = $5, isAdmin = $6, password = $7, speciality = $8 WHERE idEmployee = $9';
         const values = [firstname, lastname, job, phoneNumber, email, isAdmin, password, speciality, idEmployee];
-        pool.query(query, values, (error, result) => {
+        pool.query(query, values, (error, updateEmployee) => {
             if (error) {
                 return callback(error, null);
             }
-            callback(null, result.rowCount > 0); // Renvoie true si une ligne a été mise à jour
+            const row = updateEmployee.rows[0];
+            updateEmployee = new Employee(row.idEmployee, row.firstname, row.lastname, row.job, row.phoneNumber, row.email, row.isAdmin, row.password, row.speciality);
+            callback(null, updateEmployee);
         });
     }
 }
