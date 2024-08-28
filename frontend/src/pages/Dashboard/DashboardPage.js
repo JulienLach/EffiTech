ad9039a741a926d68554f0227ac3b920b3613e4e
JulienLach <leadedchamps@gmail.com>
2024-08-28 12:00:16 +0200

    added switch cases styles for get all event statuses

--- frontend/src/pages/Dashboard/DashboardPage.js
+++ frontend/src/pages/Dashboard/DashboardPage.js
@@ -25,6 +25,29 @@ const DashboardPage = function () {
     });
   }, []);
 
+  const getStatusIndicator = (status) => {
+    const style = {
+        padding: '2px 10px',
+        borderRadius: '8px',
+        color: 'white',
+        fontSize: '0.8em',
+        fontWeight: '500',
+    };
+
+    switch (status) {
+        case 5:
+            return <span style={{ ...style, backgroundColor: '#DCFFD6', color: '#48903C' }}>Terminé</span>;
+        case 4:
+            return <span style={{ ...style, backgroundColor: '#D3F4FF', color: '#2C5BA1' }}>Aujourd'hui</span>;
+        case 3:
+            return <span style={{ ...style, backgroundColor: '#FFDEDE', color: '#923838' }}>En retard</span>;
+        case 2:
+            return <span style={{ ...style, backgroundColor: '#FFECCF', color: '#C35E00' }}>À venir</span>;
+        case 1:
+            return <span style={{ ...style, backgroundColor: '#EBEBEB', color: '#505050' }}>À planifier</span>;
+    }
+};
+
   return (
     <div>
       <Header />
@@ -62,7 +85,7 @@ const DashboardPage = function () {
                     <td>INT-{event.idEvent}</td>
                     <td>{event.type}</td>
                     <td><a href="#">{event.title}</a></td>
-                    <td>{event.status}</td>
+                    <td>{getStatusIndicator(event.status)}</td>
                     <td>{new Date(event.startingDate).toLocaleDateString()}</td>
                     <td>
                       <a href="#">
