a902ec4e2f306dcc7c0318cdb4bfd480b43e9c0c
JulienLach <leadedchamps@gmail.com>
2024-09-29 16:59:23 +0200

    refactored TemplateGlobal to class

--- frontend/src/pages/Template/TemplateGlobal.js
+++ frontend/src/pages/Template/TemplateGlobal.js
@@ -1,72 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./TemplateGlobal.module.css";
 import logo from "../../images/logo.svg";
 import bellIcon from "../../images/notificationBell.svg";
 
-const TemplateGlobal = function () {
-    return (
-        <>
-            {/* Partie header */}
-            <div className={styles.header}>
-                <div className={styles.headerDiv}>
-                    <div className={styles.logo}>
-                        <img src={logo} alt="Effitech Logo" />
-                    </div>
-                    <div className={styles.headerRight}>
-                        <div>
-                            <button className={styles.planifierButton}>
-                                <i className="fa-solid fa-plus"></i>Créer
-                            </button>
+class TemplateGlobal extends Component {
+    render() {
+        return (
+            <>
+                {/* Partie header */}
+                <div className={styles.header}>
+                    <div className={styles.headerDiv}>
+                        <div className={styles.logo}>
+                            <img src={logo} alt="Effitech Logo" />
                         </div>
-                        <div className={styles.notificationIcon}>
-                            <img src={bellIcon} className={styles.notificationBell}></img>
-                            <span className={styles.notificationCount}>2</span>
+                        <div className={styles.headerRight}>
+                            <div>
+                                <button className={styles.planifierButton}>
+                                    <i className="fa-solid fa-plus"></i>Créer
+                                </button>
+                            </div>
+                            <div className={styles.notificationIcon}>
+                                <img src={bellIcon} className={styles.notificationBell}></img>
+                                <span className={styles.notificationCount}>2</span>
+                            </div>
+                            <div className={styles.profileBubble}>JL</div>
                         </div>
-                        <div className={styles.profileBubble}>JL</div>
                     </div>
                 </div>
-            </div>
 
-            {/* Partie sidebar */}
-            <div className={styles.sidebarAndOrange}>
-                <div className={styles.sidebar}>
-                    <ul>
-                        <li>
-                            <i className="fa-solid fa-calendar"></i>
-                            <a href="/calendar">Calendrier</a>
-                        </li>
-                        <li>
-                            <i className="fa-solid fa-user"></i>
-                            <a href="/clients">Clients</a>
-                        </li>
-                        <li>
-                            <i className="fa-solid fa-users"></i>
-                            <a href="/employees">Équipes</a>
-                        </li>
-                        <li>
-                            <i className="fa-solid fa-building"></i>
-                            <a href="/company">Société</a>
-                        </li>
-                        <li>
-                            <i className="fa-solid fa-file-lines"></i>
-                            <a href="/invoices">Facture/Devis</a>
-                        </li>
-                        <li>
-                            <i className="fa-solid fa-folder"></i>
-                            <a href="/documents">Documents</a>
-                        </li>
-                    </ul>
+                {/* Partie sidebar */}
+                <div className={styles.sidebarAndOrange}>
+                    <div className={styles.sidebar}>
+                        <ul>
+                            <li>
+                                <i className="fa-solid fa-calendar"></i>
+                                <a href="/calendar">Calendrier</a>
+                            </li>
+                            <li>
+                                <i className="fa-solid fa-user"></i>
+                                <a href="/clients">Clients</a>
+                            </li>
+                            <li>
+                                <i className="fa-solid fa-users"></i>
+                                <a href="/employees">Équipes</a>
+                            </li>
+                            <li>
+                                <i className="fa-solid fa-building"></i>
+                                <a href="/company">Société</a>
+                            </li>
+                            <li>
+                                <i className="fa-solid fa-file-lines"></i>
+                                <a href="/invoices">Facture/Devis</a>
+                            </li>
+                            <li>
+                                <i className="fa-solid fa-folder"></i>
+                                <a href="/documents">Documents</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.sideOrange}>
+                        <i className="fa-solid fa-caret-down"></i>
+                    </div>
                 </div>
-                <div className={styles.sideOrange}>
-                    <i className="fa-solid fa-caret-down"></i>
-                </div>
-            </div>
 
-            {/* partie container à copier pour les nouvelles pages */}
-            {/* <div className={styles.container}>
-            </div> */}
-        </>
-    );
-};
+                {/* partie container à copier pour les nouvelles pages */}
+                {/* <div className={styles.container}>
+                </div> */}
+            </>
+        );
+    }
+}
 
 export default TemplateGlobal;
