4957a1a21453f122829ffb8fbf08c4bc04ac27ea
JulienLach <leadedchamps@gmail.com>
2024-10-05 20:14:08 +0200

    refactored LogoDesktop component as a class and set default page to login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './pages/Login/LoginPage';
 import CreateAccountPage from './pages/CreateAccount/CreateAccountPage';
 import CalendarPage from './pages/Calendar/CalendarPage';
@@ -11,6 +11,7 @@ const App = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Navigate to="/login" />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/create-account" element={<CreateAccountPage />} />
                 <Route path="/calendar" element={<CalendarPage />} />
--- frontend/src/components/LogoDesktop/LogoDesktop.js
+++ frontend/src/components/LogoDesktop/LogoDesktop.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logoDesktop from '../../images/logo.svg';
 import GlobalStyles from '../../styles/GlobalStyles.module.css';
 import styles from './LogoDesktop.module.css';
 
-const LogoDesktop = () => {
-    return (
-        <div className={styles.logoEffitech}>
-            <img src={logoDesktop} alt="LogoDesktop" />
-        </div>
-    );
-};
+class LogoDesktop extends Component {
+    render() {
+        return (
+            <div className={styles.logoEffitech}>
+                <img src={logoDesktop} alt="LogoDesktop" />
+            </div>
+        );
+    }
+}
 
 export default LogoDesktop;
