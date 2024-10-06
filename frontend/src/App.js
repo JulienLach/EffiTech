5482708656c4197833b6a96e1a8e39080ab5e037
JulienLach <leadedchamps@gmail.com>
2024-10-06 13:59:14 +0200

    fixed with prettier format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import LoginPage from './pages/Login/LoginPage';
-import CreateAccountPage from './pages/CreateAccount/CreateAccountPage';
-import CalendarPage from './pages/Calendar/CalendarPage';
-import InterventionFormPage from './pages/InterventionForm/InterventionFormPage';
-import ReportPage from './pages/Report/ReportPage';
-import EmployeesPage from './pages/Employees/EmployeesPage';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate,
+} from "react-router-dom";
+import LoginPage from "./pages/Login/LoginPage";
+import CreateAccountPage from "./pages/CreateAccount/CreateAccountPage";
+import CalendarPage from "./pages/Calendar/CalendarPage";
+import InterventionFormPage from "./pages/InterventionForm/InterventionFormPage";
+import ReportPage from "./pages/Report/ReportPage";
+import EmployeesPage from "./pages/Employees/EmployeesPage";
 
 const App = () => {
     return (
@@ -15,7 +20,10 @@ const App = () => {
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/create-account" element={<CreateAccountPage />} />
                 <Route path="/calendar" element={<CalendarPage />} />
-                <Route path="/intervention-form" element={<InterventionFormPage />} />
+                <Route
+                    path="/intervention-form"
+                    element={<InterventionFormPage />}
+                />
                 <Route path="/report" element={<ReportPage />} />
                 <Route path="/employees" element={<EmployeesPage />} />
             </Routes>
