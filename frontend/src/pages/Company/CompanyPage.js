ef5eeb84ce3fd15f78dc68a3a26036898c6fd989
JulienLach <leadedchamps@gmail.com>
2024-10-13 18:35:25 +0200

    style(company): add separator divs to company page

--- frontend/src/pages/Company/CompanyPage.js
+++ frontend/src/pages/Company/CompanyPage.js
@@ -35,20 +35,21 @@ class CompanyPage extends Component {
                         <div className={styles.logoCompany}>
                             <img src={logoCompany} alt="Logo de la société" />
                         </div>
+                        <div className={styles.separator}></div>
                         <div>
                             <h3>Coordonnées</h3>
                             <p>Téléphone : {company.phoneNumber}</p>
                             {company.idAddress && ( // vérifier si l'adresse est définie
                                 <>
-                                    <p>Adresse : {company.idAddress.address}</p>
-                                    <p>Ville : {company.idAddress.city}</p>
                                     <p>
-                                        Code Postal :{" "}
-                                        {company.idAddress.zipcode}
+                                        Adresse : {company.idAddress.address},{" "}
+                                        {company.idAddress.zipcode} ,
+                                        {company.idAddress.city}
                                     </p>
                                 </>
                             )}
                         </div>
+                        <div className={styles.separator}></div>
                         <div>
                             <h3>Informations</h3>
                             <p>SIRET : {company.siret}</p>
