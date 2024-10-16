56857f1186cc299432075b1d64449171af4cd66e
JulienLach <leadedchamps@gmail.com>
2024-10-16 21:06:45 +0200

    feat(company: add updateCompany method to data

--- backend/data/company.data.js
+++ backend/data/company.data.js
@@ -50,6 +50,47 @@ class Company {
             });
         });
     }
+
+    static updateCompany(company, callback) {
+        const query =
+            "UPDATE companies SET phone_number = $1, id_address = $2, siret = $3, vat_number = $4, capital = $5, logo = $6, database_version = $7 WHERE id_company = $8 RETURNING *";
+        const values = [
+            company.phoneNumber,
+            company.idAddress,
+            company.siret,
+            company.vatNumber,
+            company.capital,
+            company.logo,
+            company.databaseVersion,
+            company.idCompany,
+        ];
+
+        pool.query(query, values, (error, result) => {
+            if (error) {
+                return callback(error);
+            }
+
+            const row = result.rows[0];
+
+            Address.getAddressById(row.id_address, function (error, address) {
+                if (error) {
+                    return callback(error, null);
+                }
+
+                let company = new Company(
+                    row.id_company,
+                    row.phone_number,
+                    address,
+                    row.siret,
+                    row.vat_number,
+                    row.capital,
+                    row.logo,
+                    row.database_version
+                );
+                callback(null, company);
+            });
+        });
+    }
 }
 
 module.exports = Company;
