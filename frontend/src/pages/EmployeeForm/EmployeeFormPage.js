139e47adfe30ab693a16eea90a38b0cc1a34bc66
JulienLach <leadedchamps@gmail.com>
2024-10-20 08:39:44 +0200

    feat(employee): add employee data to EmployeeFormPage

--- frontend/src/pages/EmployeeForm/EmployeeFormPage.js
+++ frontend/src/pages/EmployeeForm/EmployeeFormPage.js
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
+import { useLocation } from "react-router-dom";
 import TemplateGlobal from "../Template/TemplateGlobal";
 import styles from "./EmployeeFormPage.module.css";
 import profilPicture from "../../images/profil.png";
+import { getEmployeeById } from "../../services/api";
+
+// Composant wrapper pour utiliser les hooks
+function EmployeeFormPageWrapper() {
+    const location = useLocation();
+    return <EmployeeFormPage location={location} />;
+}
 
 class EmployeeFormPage extends Component {
+    constructor(props) {
+        super(props);
+        const { employee } = this.props.location.state;
+        this.state = {
+            employee: employee,
+            idEmployee: employee.idEmployee,
+            error: null,
+        };
+    }
+
+    componentDidMount() {
+        const { idEmployee } = this.state;
+        getEmployeeById(idEmployee, (error, data) => {
+            if (error) {
+                this.setState({ error: error.message });
+            } else {
+                this.setState({ employee: data });
+            }
+        });
+    }
+
     render() {
+        const { employee } = this.state;
+
         return (
             <>
                 <TemplateGlobal />
@@ -16,35 +47,27 @@ class EmployeeFormPage extends Component {
                     <form className={styles.formElements}>
                         <div className={styles.labelInput}>
                             <label htmlFor="lastname">Nom:</label>
-                            <input type="text" id="lastname" name="lastname" />
+                            <input type="text" value={employee.lastname} />
                         </div>
                         <div className={styles.labelInput}>
                             <label htmlFor="firstname">Prénom:</label>
-                            <input
-                                type="text"
-                                id="firstname"
-                                name="firstname"
-                            />
+                            <input type="text" value={employee.firstname} />
                         </div>
                         <div className={styles.labelInput}>
                             <label htmlFor="job">Métier:</label>
-                            <input type="text" id="job" name="job" />
+                            <input type="text" value={employee.job} />
                         </div>
                         <div className={styles.labelInput}>
                             <label htmlFor="speciality">Spécialité:</label>
-                            <input
-                                type="text"
-                                id="speciality"
-                                name="speciality"
-                            />
+                            <input type="text" value={employee.speciality} />
                         </div>
                         <div className={styles.labelInput}>
                             <label htmlFor="mail">Adresse mail:</label>
-                            <input type="email" id="mail" name="mail" />
+                            <input type="email" value={employee.email} />
                         </div>
                         <div className={styles.labelInput}>
                             <label htmlFor="phone">Téléphone:</label>
-                            <input type="text" id="phone" name="phone" />
+                            <input type="text" value={employee.phoneNumber} />
                         </div>
                     </form>
                     <div className={styles.buttonPosition}>
@@ -69,4 +92,4 @@ class EmployeeFormPage extends Component {
     }
 }
 
-export default EmployeeFormPage;
+export default EmployeeFormPageWrapper;
