18d0de0a780e081d818c5c7620aab4c565df131e
JulienLach <leadedchamps@gmail.com>
2024-11-08 14:30:21 +0100

    fix(employee): refactor employees page with grid, fix phoneNumber field in employeeForm and add auth to createEmployee api call

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -695,6 +695,7 @@ function createClient(clientData, callback) {
 function createEmployee(employeeData, callback) {
     const xhr = new XMLHttpRequest();
     xhr.open("POST", `${API_URL}/employees`);
+    xhr.withCredentials = true;
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onload = function () {
         if (xhr.status === 200 || xhr.status === 201) {
