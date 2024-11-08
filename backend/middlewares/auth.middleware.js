efb2e068c99939ed58201a91731256743ae1e192
JulienLach <leadedchamps@gmail.com>
2024-11-08 10:40:58 +0100

    feat(auth): add auth middleware to getAllEvent event route and and jwt payload check to auth middleware with idEmployee and isAdmin properties

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -19,11 +19,21 @@ function authenticateToken(req, res, next) {
         // return res.redirect("http://localhost:3000/login");
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
+            console.log("Erreur de vérification du token:", err);
             return res.status(403).json({ message: "Token invalide." });
         }
-        req.user = user;
+        console.log("Payload décodé:", decoded);
+
+        req.employee = {
+            idEmployee: decoded.idEmployee,
+            isAdmin: decoded.isAdmin,
+        };
+        console.log(
+            "Informations de l'employé ajoutées à req.employee:",
+            req.employee
+        );
         next();
     });
 }
