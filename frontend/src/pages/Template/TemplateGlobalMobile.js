211bf22942459e5ca3fc16ad397291e0dfeabf85
JulienLach <leadedchamps@gmail.com>
2024-11-24 08:46:45 +0100

    feat(mobile): add employee initials to templateGlobalMobile

--- frontend/src/pages/Template/TemplateGlobalMobile.js
+++ frontend/src/pages/Template/TemplateGlobalMobile.js
@@ -4,7 +4,35 @@ import logo from "../../images/logo.svg";
 import bellIcon from "../../images/notificationBell.svg";
 
 class TemplateGlobalMobile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPath: window.location.pathname,
+            showProfileMenu: false,
+            initials: "",
+        };
+        this.setInitials = this.setInitials.bind(this);
+    }
+
+    componentDidMount() {
+        this.setInitials();
+    }
+
+    setInitials() {
+        const firstname = localStorage.getItem("firstname");
+        const lastname = localStorage.getItem("lastname");
+
+        if (firstname && lastname) {
+            const initials = `${firstname.charAt(0)}${lastname.charAt(
+                0
+            )}`.toUpperCase();
+            this.setState({ initials });
+        }
+    }
+
     render() {
+        const { initials } = this.state;
+
         return (
             <>
                 {/* Partie header */}
@@ -19,7 +47,7 @@ class TemplateGlobalMobile extends Component {
                             ></img>
                             <span className={styles.notificationCount}>2</span>
                         </div>
-                        <div className={styles.profileBubble}>JL</div>
+                        <div className={styles.profileBubble}>{initials}</div>
                     </div>
                 </div>
 
