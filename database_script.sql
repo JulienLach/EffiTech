6082263e304d6556bf31245ea238e8d181ae9c6c
JulienLach <leadedchamps@gmail.com>
2024-12-04 15:26:13 +0100

    script(sql): update SQL script to add test employee

--- database_script.sql
+++ database_script.sql
@@ -52,7 +52,7 @@ CREATE TABLE notifications(
 
 CREATE TABLE addresses(
    id_address SERIAL PRIMARY KEY,
-   address_street VARCHAR(100) NOT NULL,
+   address VARCHAR(100) NOT NULL,
    zipcode VARCHAR(50) NOT NULL,
    city VARCHAR(50) NOT NULL,
    id_client INT,
@@ -106,3 +106,7 @@ CREATE TABLE companies(
    UNIQUE(id_address),
    FOREIGN KEY(id_address) REFERENCES addresses(id_address)
 );
+
+-- Insérer un utilisateur par défaut
+INSERT INTO employees (firstname, lastname, job, phone_number, email, is_admin, password, speciality)
+VALUES ('test', 'test', '', '', 'test@gmail.com', true, 'ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff', '');
