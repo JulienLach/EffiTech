bebff7856dd31b9b4e3cdf7275ae8d8d1678b814
JulienLach <leadedchamps@gmail.com>
2025-01-12 19:20:14 +0100

    fix(notifications): remove notification count on mobile

--- frontend/src/pages/Template/TemplateGlobalMobile.js
+++ frontend/src/pages/Template/TemplateGlobalMobile.js
@@ -47,6 +47,9 @@ class TemplateGlobalMobile extends Component {
             case "/expense":
                 pageTitle = "Dépenses";
                 break;
+            case "/report":
+                pageTitle = "Rapport";
+                break;
             default:
                 pageTitle = "Page";
         }
@@ -71,9 +74,9 @@ class TemplateGlobalMobile extends Component {
                                     className={styles.notificationBell}
                                     alt="Notification Bell"
                                 />
-                                <span className={styles.notificationCount}>
-                                    2
-                                </span>
+                                <span
+                                    className={styles.notificationCount}
+                                ></span>
                             </div>
                             <div className={styles.profileBubble}>
                                 {initials}
