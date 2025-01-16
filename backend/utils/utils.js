7e4d3ac63fb0c629d14c893931cb4e6e26d8f982
JulienLach <leadedchamps@gmail.com>
2025-01-16 09:05:45 +0100

    fix(docs): fix README.md and add comments to utils class

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,9 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 
+// Pour passer n'importe quel argument à la méthode addLog
+// Exemple : Utils.addLog("Erreur : " + error.message); ou Utils.addLog("Message", employee.name);
 class Utils {
     static addLog(message) {
+        // Définit le chemin du dossier logs et du fichier log.txt à partir du dossier courant
         const logDir = path.join(__dirname, "logs");
+        // définir le chemin du fichier log.txt à partir du dossier logs
+        // grace à la méthode path.join qui permet de concaténer les chemins
         const logFile = path.join(logDir, "log.txt");
 
         // Vérifie si le dossier logs existe, sinon le crée
